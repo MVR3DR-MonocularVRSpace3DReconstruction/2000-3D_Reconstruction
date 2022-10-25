ce11495aee081bcb276393818b1b50366c957fea
our0boros <863166709@qq.com>
2022-10-25 19:35:38 +0800

    video

--- frames2video.py
+++ frames2video.py
@@ -3,15 +3,15 @@ import numpy as np
 import glob
 from tqdm import tqdm
 img_array = []
-img_dir = sorted(glob.glob("data/redwood-livingroom/image/*.jpg"))
-for filename in img_dir[:len(img_dir)//8]:
+img_dir = sorted(glob.glob("data/redwood-boardroom/image/*.jpg"))
+for filename in img_dir: # [:len(img_dir)//8]
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
 
 
-out = cv2.VideoWriter('livingroom_01.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+out = cv2.VideoWriter('boardroom.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
  
 for i in tqdm(range(len(img_array))):
     out.write(img_array[i])
